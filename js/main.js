d1f2ae51925ecbbdab19bbcf0b7a2e27c56298c0
Horoiu Cezar <horoiucezar@yahoo.comgit config --global user.name Horoiu Cezargit statusrungit ipgit config --global user.email horoiucezar@yahoo.com>
2023-07-21 11:48:39 +0300

    #day3 edit code

--- js/main.js
+++ js/main.js
@@ -53,7 +53,7 @@ function startCount(el) {
   let goal = el.dataset.goal;
   let count = setInterval(() => {
     el.textContent++;
-    if (el.textContent == goal) {
+    if (el.textContent === goal) {
       clearInterval(count);
     }
   }, 2000 / goal);
